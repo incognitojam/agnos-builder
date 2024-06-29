71a5a90a7c9ca931290471626e6324cd08db1de9
Cameron Clough <cameronjclough@gmail.com>
2024-06-29 22:58:40 +0100

    Revert "try this"
    This reverts commit 0ca56905083e59ba7913b5670bc5284061853ed2.

--- userspace/openpilot_python_dependencies.sh
+++ userspace/openpilot_python_dependencies.sh
@@ -18,4 +18,4 @@ fi
 echo "Setting global python version"
 pyenv global $PYTHON_VERSION
 
-pip3 install --no-cache-dir --upgrade pip poetry setuptools wheel
+pip3 install --no-cache-dir --upgrade pip poetry
